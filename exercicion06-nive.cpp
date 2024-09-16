d0ea9f64b74b4e0d9ca6a8d108fc5625d491ceae
biah24 <bianca20062406@gmail.com>
2024-09-16 18:51:01 -0300

    Add files via upload

---
+++ exercicion06-nive.cpp
@@ -0,0 +1,30 @@
+//cos=1 compara com o cos, se for ihual ta certo se não for errado, diferente de multiplo de 4, (-) por 2 (+), i = real
+
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+#define PI = 3.14
+
+int main ();
+	float fat, x, num, soma =1, valor, 
+	int = i, f
+	x = ((x * PI)/180;)
+	printf("Digite o valor de X: ");
+	scanf("%f", &x);
+	
+	
+	for(i = 1; i <= 8; i++){
+		if (i%2 == 0);
+			soma = soma - valor;
+		else 
+			soma = soma + valor;
+			
+	}for (f =1; <=8; f++);
+		if (f%2 == 0);
+			soma = soma - valor;
+		else 
+			soma = soma + valor;
+		
+			
+	
+	
